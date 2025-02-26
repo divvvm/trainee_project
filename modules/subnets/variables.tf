8171df4e92c3c8167e410f75e5d53fcf92b24bb6
Denys Misiailo <dmisi@softserveinc.com>
2025-02-26 14:07:39 +0200

    Changed project structure and modified CIDR block

--- modules/subnets/variables.tf
+++ modules/subnets/variables.tf
@@ -1,35 +1,39 @@
-variable "vpc_cidr" {
-  description = "CIDR block for the VPC"
+variable "vpc_id" {
+  description = "ID of the VPC"
   type        = string
-  default     = "10.0.0.0/24"
 }
 
 variable "public_subnet_cidr_1" {
   description = "CIDR block for public subnet 1 (Bastion)"
   type        = string
-  default     = "10.0.0.0/27"
+  default     = "10.0.0.0/28"
 }
 
 variable "public_subnet_cidr_2" {
   description = "CIDR block for public subnet 2 (ALB/NAT)"
   type        = string
-  default     = "10.0.0.32/27"
+  default     = "10.0.0.16/28"
 }
 
 variable "private_app_subnet_cidr_1" {
   description = "CIDR block for private app subnet 1 (LLM1)"
   type        = string
-  default     = "10.0.0.64/26"
+  default     = "10.0.0.32/27"
 }
 
 variable "private_app_subnet_cidr_2" {
   description = "CIDR block for private app subnet 2 (LLM2)"
   type        = string
-  default     = "10.0.0.128/26"
+  default     = "10.0.0.64/27"
 }
 
 variable "private_db_subnet_cidr" {
   description = "CIDR block for private DB subnet (PostgreSQL)"
   type        = string
-  default     = "10.0.0.192/26"
+  default     = "10.0.0.96/28"
+}
+
+variable "public_route_table_id" {
+  description = "ID of the public route table"
+  type        = string
 }
