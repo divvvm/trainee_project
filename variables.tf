ff1a74f6c836c654c23733225dd663f1052e66b9
Denys Misiailo <dmisi@softserveinc.com>
2025-02-27 10:28:34 +0200

    Format variables

--- variables.tf
+++ variables.tf
@@ -1,23 +1,23 @@
 variable "vpc_cidr" {
-  default     = "10.0.0.0/24"
+  default = "10.0.0.0/24"
 }
 
 variable "public_subnet_cidr_1" {
-  default     = "10.0.0.0/28"
+  default = "10.0.0.0/28"
 }
 
 variable "public_subnet_cidr_2" {
-  default     = "10.0.0.16/28"
+  default = "10.0.0.16/28"
 }
 
 variable "private_app_subnet_cidr_1" {
-  default     = "10.0.0.32/27"
+  default = "10.0.0.32/27"
 }
 
 variable "private_app_subnet_cidr_2" {
-  default     = "10.0.0.64/27"
+  default = "10.0.0.64/27"
 }
 
 variable "private_db_subnet_cidr" {
-  default     = "10.0.0.96/28"
+  default = "10.0.0.96/28"
 }
