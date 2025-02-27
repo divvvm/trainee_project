8160d49d2024f2da05330b9a162bab896ce9355a
Denys Misiailo <dmisi@softserveinc.com>
2025-02-27 10:26:03 +0200

    Modify variables

--- modules/subnets/variables.tf
+++ modules/subnets/variables.tf
@@ -6,31 +6,26 @@ variable "vpc_id" {
 variable "public_subnet_cidr_1" {
   description = "CIDR block for public subnet 1 (Bastion)"
   type        = string
-  default     = "10.0.0.0/28"
 }
 
 variable "public_subnet_cidr_2" {
   description = "CIDR block for public subnet 2 (ALB/NAT)"
   type        = string
-  default     = "10.0.0.16/28"
 }
 
 variable "private_app_subnet_cidr_1" {
   description = "CIDR block for private app subnet 1 (LLM1)"
   type        = string
-  default     = "10.0.0.32/27"
 }
 
 variable "private_app_subnet_cidr_2" {
   description = "CIDR block for private app subnet 2 (LLM2)"
   type        = string
-  default     = "10.0.0.64/27"
 }
 
 variable "private_db_subnet_cidr" {
   description = "CIDR block for private DB subnet (PostgreSQL)"
   type        = string
-  default     = "10.0.0.96/28"
 }
 
 variable "public_route_table_id" {
--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,5 +1,4 @@
 variable "vpc_cidr" {
   description = "CIDR block for the VPC"
   type        = string
-  default     = "10.0.0.0/24"
 }
