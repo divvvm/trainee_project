016c797d3029cb45da82a2051dc9a653a2c4cabb
Denys Misiailo <dmisi@softserveinc.com>
2025-03-04 12:36:04 +0200

    Fix engine version of RDS

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -15,7 +15,7 @@ resource "aws_db_instance" "main" {
   db_name                = var.db_name
   username               = var.db_username
   password               = var.db_password
-  parameter_group_name   = "default.postgres14"
+  engine_version         = "16.3"
   vpc_security_group_ids = [var.security_group_id]
   db_subnet_group_name   = aws_db_subnet_group.main.name
   publicly_accessible    = false
