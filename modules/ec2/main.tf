ab8c426736c0b645bba2113f04842061f02333c7
Denys Misiailo <dmisi@softserveinc.com>
2025-03-05 15:10:36 +0200

    Update Instance type and volumes

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -25,8 +25,8 @@ resource "aws_launch_template" "asg" {
   block_device_mappings {
     device_name = "/dev/xvda"
     ebs {
-      volume_size = var.ebs_volume_size
-      volume_type = "gp2"
+      volume_size           = var.ebs_volume_size
+      volume_type           = "gp2"
       delete_on_termination = true
     }
   }
